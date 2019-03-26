a96120f7b9296b25fa0d54ef87be0b08d1f92ece
Eric Vera <hey@itsmeeric.com>
2019-03-26 15:28:14 -0700

    Add props to TextField

--- packages/material-ui-bonus/src/TextField.js
+++ packages/material-ui-bonus/src/TextField.js
@@ -10,16 +10,34 @@ const TextField = ({
   label,
   placeholder,
   marginTopLevel = '0',
-  marginBottomLevel = '0'
+  marginBottomLevel = '0',
+  disabled,
+  onChange,
+  value,
+  id,
+  autoFocus,
+  type,
+  onBlur,
+  error,
+  helperText
 }) => {
   const marginClassName = useMarginStyles(marginTopLevel, marginBottomLevel)
 
   return (
     <MaterialUITextField
       fullWidth={true}
+      disabled={disabled}
+      value={value}
+      onChange={onChange}
       label={label}
       placeholder={placeholder}
       className={marginClassName}
+      id={id}
+      autoFocus={autoFocus}
+      type={type}
+      onBlur={onBlur}
+      error={error}
+      helperText={helperText}
     />
   )
 }
@@ -27,6 +45,15 @@ const TextField = ({
 TextField.propTypes = {
   placeholder: PropTypes.string.isRequired,
   label: PropTypes.string,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  value: PropTypes.string,
+  id: PropTypes.string,
+  autoFocus: PropTypes.bool,
+  type: PropTypes.string,
+  onBlur: PropTypes.func,
+  error: PropTypes.bool,
+  helperText: PropTypes.string,
   marginTopLevel: PropTypes.oneOf(['0', '1', '2', '3', '4', '5', '6', '7']),
   marginBottomLevel: PropTypes.oneOf(['0', '1', '2', '3', '4', '5', '6', '7'])
 }
--- packages/material-ui-bonus/src/TextField.stories.js
+++ packages/material-ui-bonus/src/TextField.stories.js
@@ -3,10 +3,20 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import TextField from './TextField'
 
-storiesOf('TextField', module).add('default', () => (
-  <TextField
-    label="Label here"
-    placeholder="Placeholder text"
-    onChange={action('text changed')}
-  />
-))
+storiesOf('TextField', module)
+  .add('default', () => (
+    <TextField
+      label="Label here"
+      placeholder="Placeholder text"
+      onChange={action('text changed')}
+    />
+  ))
+  .add('disabled', () => (
+    <TextField
+      disabled={true}
+      value="Unchangeable value"
+      label="Label here"
+      placeholder="Placeholder text"
+      onChange={action('text changed')}
+    />
+  ))
